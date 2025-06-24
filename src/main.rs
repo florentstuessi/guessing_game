31a655e7c8d39d77a1c715df0b473be8d1f10f32
Florent Stuessi <florentstuessi@hotmail.com>
2025-06-24 14:05:25 +0200

    Update rand crate to v0.9.0

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ fn main() {
     // thread_rng() is a function that provides a random number generator
     // gen_range(1..=100) generates a random number in the range 1 to 100
     // The `=` in the range means that 100 is inclusive
-    let secret_number = rand::thread_rng()
-                                .gen_range(1..=100);
+    let secret_number = rand::rng()
+                                .random_range(1..=100);
 
     println!("The secret number is: {secret_number}");
 
