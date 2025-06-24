876951630484c635dc911186d7a3c11ea2267336
Florent Stuessi <florentstuessi@hotmail.com>
2025-06-24 14:23:48 +0200

    Setup match expression

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 
 use rand::Rng;
 
@@ -14,11 +15,20 @@ fn main() {
     println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
+
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
